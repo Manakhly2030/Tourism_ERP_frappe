e41a2caf67d90fa8dcec5af2e7c29f5a788a84e4
aditishah24 <aditiph1@gmail.com>
2024-02-13 17:18:16 +0530

    Added Contact Info Doctype

---
+++ tourism_erp/tourism_erp/doctype/contact_information/contact_information.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Technovate and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Contact Information", {
+// 	refresh(frm) {
+
+// 	},
+// });
