53a16821e53ca4fddfd2d3192e671278a6e53843
PrathamShah3011 <pratham3011@gmail.com>
2024-02-28 17:07:09 +0530

    tourism_customer validations

--- tourism_erp/tourism_erp/doctype/tourism_customer/tourism_customer.js
+++ tourism_erp/tourism_erp/doctype/tourism_customer/tourism_customer.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2024, Technovate and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tourism Customer", {
-// 	refresh(frm) {
+frappe.ui.form.on("Tourism Customer", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    validate: function(frm){
+        //check for duplicate documents
+        var documents = frm.doc.document;
+        var documentTypes = new Set();
+    
+        for(let i=0; i<documents.length; i++){
+            if(documentTypes.has(documents[i].document_type)){
+                frappe.throw('Duplicate document found: ' + documents[i].document_type);
+            }
+            documentTypes.add(documents[i].document_type);
+        }
+
+        // full name
+        frm.set_value('full_name', frm.doc.first_name + ' ' + frm.doc.last_name);
+
+        //age
+        // if (frm.doc.dob) {
+        //     var today = new Date();
+        //     var birthDate = new Date(frm.doc.dob);
+        //     console.log(birthDate);
+        //     var age = today.getFullYear() - birthDate.getFullYear();
+        //     var m = today.getMonth() - birthDate.getMonth();
+        //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        //         age--;
+        //     }
+        //     frm.set_value('age', age);
+        // }
+    },
+    }
+);
--- tourism_erp/tourism_erp/doctype/tourism_customer/tourism_customer.py
+++ tourism_erp/tourism_erp/doctype/tourism_customer/tourism_customer.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, Technovate and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
-
+from frappe.utils import getdate, today, now_datetime, now
 class TourismCustomer(Document):
-	pass
+	# def validate(self):
+	@property
+	def age(self):
+		today = getdate()
+		dob = getdate(self.dob)
+		return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+	# pass
