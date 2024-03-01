b565fb30e79814ac0947a2ae2a59893e57b45031
lynx-gh <76166621+LynX-gh@users.noreply.github.com>
2024-03-01 11:47:03 +0530

    Added Contact Information Validation
    Signed-off-by: lynx-gh <76166621+LynX-gh@users.noreply.github.com>

--- tourism_erp/tourism_erp/doctype/contact_information/contact_information.js
+++ tourism_erp/tourism_erp/doctype/contact_information/contact_information.js
@@ -1,8 +1,47 @@
 // Copyright (c) 2024, Technovate and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contact Information", {
-// 	refresh(frm) {
+frappe.ui.form.on("Contact Information", {
+  refresh(frm) {
+    frm.fields_dict["contact_numbers"].grid.grid_rows[0].toggle_display("phone", 0);
+    frm.fields_dict["contact_numbers"].grid.grid_rows[0].toggle_display("phone", 1);
+  },
+});
 
-// 	},
-// });
+frappe.ui.form.on("Contact Phone", {
+  is_primary_phone(frm, cdt, cdn) {
+    const row = frappe.get_doc(cdt, cdn);
+    const primary_set = row.is_primary_phone ? 0 : 1;
+    if (row.is_primary_phone) {
+      frm.doc.primary_contact_no = row.phone;
+      frm.refresh_field("primary_contact_no");
+    }
+    for (const [doc_name, grid] of Object.entries(
+      frm.fields_dict["contact_numbers"].grid.grid_rows_by_docname
+    )) {
+      if (doc_name !== row.name) {
+        grid.toggle_display("is_primary_phone", primary_set);
+      }
+    }
+    frm.refresh_field("contact_numbers");
+  },
+});
+
+frappe.ui.form.on("Contact Email", {
+  is_primary(frm, cdt, cdn) {
+    const row = frappe.get_doc(cdt, cdn);
+    const primary_set = row.is_primary ? 0 : 1;
+    if (row.is_primary) {
+      frm.doc.primary_email_id = row.email_id;
+      frm.refresh_field("primary_email_id");
+    }
+    for (const [doc_name, grid] of Object.entries(
+      frm.fields_dict["email_ids"].grid.grid_rows_by_docname
+    )) {
+      if (doc_name !== row.name) {
+        grid.toggle_display("is_primary", primary_set);
+      }
+    }
+    frm.refresh_field("email_ids");
+  },
+});
--- tourism_erp/tourism_erp/doctype/contact_information/contact_information.py
+++ tourism_erp/tourism_erp/doctype/contact_information/contact_information.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2024, Technovate and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import validate_email_address, validate_phone_number
 from frappe.model.document import Document
 
 
 class ContactInformation(Document):
-	pass
+    def validate(self):
+        self.validate_email()
+        self.validate_phone()
+        frappe.throw('Validation Error')
+
+    def validate_email(self):
+        for email in [email_doc.email_id for email_doc in self.get('email_ids')]:
+            if not validate_email_address(email, throw=True):
+                frappe.throw('Email Id is not valid')
+
+    def validate_phone(self):
+        for phone in [contact_doc.phone for contact_doc in self.get('contact_numbers')]:
+            frappe.log(phone)
+            if not validate_phone_number(phone, throw=True):
+                frappe.throw('Phone number is not valid')
