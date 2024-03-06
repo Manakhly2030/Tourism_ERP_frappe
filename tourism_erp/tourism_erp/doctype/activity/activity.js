2635c71b8cb67f21c82f457c128b8c06304131b3
aditishah24 <aditiph1@gmail.com>
2024-03-06 15:37:15 +0530

    Validations in Travel Agency and modification in Activity

--- tourism_erp/tourism_erp/doctype/activity/activity.js
+++ tourism_erp/tourism_erp/doctype/activity/activity.js
@@ -13,26 +13,48 @@ frappe.ui.form.on("Activity", {
         }
 
         // Price per person validation
-        if (frm.doc.price_per_person <= 0) {
-            frappe.msgprint("Price per person cannot be Zero");
+        if (frm.doc.price_per_person < 0) {
+            frappe.msgprint("Price per person cannot be Negative");
             frappe.validated = false;
             return;
         }
 
-        //Description
+        Description
         if (frm.doc.description && frm.doc.description.length > 1000){
             frappe.msgprint("Description is too long. Maximum 1000 characters allowed.");
             frappe.validated = false;
             return;
         }
 
-        //Opening/Closing Time
-        if (frm.doc.opening_time >= frm.doc.closing_time) {
-            frappe.msgprint("Closing time must be after opening time");
+    //     //Opening/Closing Time
+    
+     var openingTime = frm.doc.opening_time;
+        var closingTime = frm.doc.closing_time;
+
+        // Extract hour parts from opening and closing times
+        var openingHour = parseInt(openingTime.split(':')[0], 10);
+        var closingHour = parseInt(closingTime.split(':')[0], 10);
+
+        // Check if opening hour is strictly equal to closing hour
+        if (openingHour === closingHour) {
+            frappe.msgprint("Opening and closing times cannot have the same hour");
             frappe.validated = false;
             return;
         }
+        if (openingTime >= closingTime) {
+            frappe.msgprint("Closing time must be after Opening time");
+            frappe.validated = false;
+            return;
+        }
+    
+        // if (frm.doc.opening_time === frm.doc.closing_time) {
+        //     frappe.msgprint("Opening and closing times cannot be the same");
+        //     frappe.validated = false;
+        //     return;
+        // }
     },
+         
+    
 
     before_save: function(frm) {
         // Activity Name validation
--- tourism_erp/tourism_erp/doctype/travel_agency/travel_agency.js
+++ tourism_erp/tourism_erp/doctype/travel_agency/travel_agency.js
@@ -1,8 +1,68 @@
 // Copyright (c) 2024, Technovate and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Travel Agency", {
-// 	refresh(frm) {
+frappe.ui.form.on("Travel Agency", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+
+
+    validate: function(frm){
+
+        if (!frm.doc.address) {
+            frappe.msgprint("Please enter address.");
+            frappe.validated = false;
+            return;
+        }
+
+        if (!frm.doc.license_number) {
+            frappe.msgprint("Please enter license number.");
+            frappe.validated = false;
+            return;
+        }
+        
+        
+        var licenseNumberPattern = /^[A-Z0-9]+$/;
+        if (!licenseNumberPattern.test(frm.doc.license_number)) {
+            frappe.msgprint("Please enter a valid license number.");
+            frappe.validated = false;
+            return;
+        }
+
+        if(!frm.doc.agency_website){
+            frappe.msgprint("Please enter Agency Website")
+            frappe.validated = false;
+            return;
+        } 
+        if (!frm.doc.agency_website.match(/^(http|https):\/\/[^ "]+$/)) {
+            frappe.msgprint(__("Please enter a valid website URL."));
+            frappe.validated = false;
+            return;
+        }
+        //select contact-info before contact no or email
+        if (!frm.doc.contact_information) {
+            frappe.msgprint(__("Please enter contact information first."));
+            frappe.validated = false;
+            return;
+        }
+
+        
+        if (!frm.doc.contact_no) {
+            frappe.msgprint(__("Please enter contact number."));
+            frappe.validated = false;
+            return;
+        }
+
+        
+        if (!frm.doc.email) {
+            frappe.msgprint(__("Please enter email."));
+            frappe.validated = false;
+            return;
+        }
+
+        },
+
+        
+        
+    
+});
