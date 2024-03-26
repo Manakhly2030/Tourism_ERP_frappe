88ebd0dcf63916d1b422014996857a0e34c7bd3d
aditishah24 <aditiph1@gmail.com>
2024-03-26 13:54:22 +0530

    Validation of website url in Travel agency

--- tourism_erp/tourism_erp/doctype/travel_agency/travel_agency.py
+++ tourism_erp/tourism_erp/doctype/travel_agency/travel_agency.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2024, Technovate and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+# from frappe.model.document import Document
 
+
+# class TravelAgency(Document):
+# 	pass
+
+from frappe.model.document import Document
+from frappe import _
 
 class TravelAgency(Document):
-	pass
+    def validate(self):
+        if self.agency_website:
+            import re
+            if not re.match(r".+\.com$", self.agency_website):
+                frappe.throw(_("Enter a valid URL"))
